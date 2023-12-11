221222988a0e9716715cb41bc23b7171b5f09f6b
Anton Seleznev <yqks@icloud.com>
2023-12-11 20:33:41 +0700

    second commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,7 +11,7 @@ const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
     const path =
     process.env.NODE_ENV === 'development'
       ? `http://localhost:3001/api/${endpoint}`
-      : `https://raw.githubusercontent.com/rodinesta/github-pages/github-pages/static/db/${endpoint}.json`;
+      : `https://raw.githubusercontent.com/rodinesta/github-pages/master/static/db/${endpoint}.json`;
   const response = await fetch(path);
 
   return await response.json();
